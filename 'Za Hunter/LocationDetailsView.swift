a7bf7e15d448695f60dc9f78292a632dcb3e9860
tyler0611 <berlity0611@gmail.com>
2025-02-21 09:55:35 -0600

    fixed LocationDetailsView

--- 'Za Hunter/LocationDetailsView.swift
+++ 'Za Hunter/LocationDetailsView.swift
@@ -12,7 +12,7 @@ struct LocationDetailsView: View {
     let mapItem: MKMapItem
     var body: some View {
         let address = mapItem.placemark.subThoroughfare! + " " +
-        mapItem.placemark.subThoroughfare! + "\n" +
+        mapItem.placemark.thoroughfare! + "\n" +
         mapItem.placemark.locality! + ", " +
         mapItem.placemark.administrativeArea! + " " +
         mapItem.placemark.postalCode!
